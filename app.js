9d5835173dcfadb139692c53f980586dd3b3092b
LearnProcessing <rinaldiadrian5@gmail.com>
2021-02-19 03:44:03 +0700

    socketIO feature fail

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ const cors = require('cors')
 const router = require ('./routers/index')
 const port = 3000
 const http = require('http').Server(app)
-const io = require('socket.io')(http)
+const io = require('socket.io')(http, {
+    cors: {
+        origin: 'http://localhost:8080',
+        methods: ['GET', 'POST'],
+        credentials: true
+    },
+    allowEIO3: true
+})
 
 io.on('connection',(socket) => {
     console.log('a user connected')
+    socket.on("newMessage", (data) => {
+        socket.broadcast.emit('serverMessage', data)
+    })
 })
 
 app.use(cors())
